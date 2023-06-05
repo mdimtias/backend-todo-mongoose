3a465a585169f12e0b1f5ab32bc475c9c40bdb5c
mdimtias <developertanbir@gmail.com>
2023-06-05 15:22:38 +0600

    Create Custom Instance method, static method and query helper

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -9,96 +9,126 @@ const router = express.Router();
 
 // GET ALL THE TODO
 router.get("/", checkLogin, async (req, res) => {
-    try {
-        const todos =await Todo.find({status: "active"}).populate("user", "name username -_id");
-        res.status(200).send(todos)
-    } catch (err) {
-        res.status(500).send({ error: "There was a server side error!" })
-    }
+  try {
+    const todos = await Todo.find({ status: "active" }).populate("user", "name username -_id");
+    res.status(200).send(todos)
+  } catch (err) {
+    res.status(500).send({ error: "There was a server side error!" })
+  }
 })
 
+// GET ACTIVE TODO
+router.get("/active", checkLogin, async (req, res) => {
+  try {
+    const todo = new Todo();
+    const data = await todo.findActive()
+    res.status(200).send(data)
+  } catch (err) {
+    res.status(500).send({ error: "There was a server side error!" })
+  }
+})
+
+// GET ACTIVE TODO with Callback
+router.get("/active-callback", checkLogin, async (req, res) => {
+  const todo = new Todo();
+  todo.findActiveCallback((err, data) => {
+    res.status(200).send(data)
+  })
+})
+
+// Find Title js 
+router.get("/js", checkLogin, async (req, res) => {
+  const data = await Todo.findByJs();
+  res.status(200).send({ data: data })
+})
+
+// Find Title js 
+router.get("/language", checkLogin, async (req, res) => {
+  const data = await Todo.find().byLanguage("Learn");
+  res.status(200).send({ data: data })
+})
 // GET A TODO ID
 router.get("/:id", async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-      const todo = await Todo.findById(id);
-  
-      if (!todo) {
-        return res.status(404).send({ error: 'Todo not found' });
-      }
-  
-      res.status(200).send(todo);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: 'There was a server side error!' });
+  try {
+    const todo = await Todo.findById(id);
+
+    if (!todo) {
+      return res.status(404).send({ error: 'Todo not found' });
     }
+
+    res.status(200).send(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'There was a server side error!' });
+  }
 })
 
 // POST TODO
 router.post("/", checkLogin, async (req, res) => {
-    try {
-        const newTodo = new Todo({
-          ...req.body,
-          user: req.userId
-        });
-        const todo = await newTodo.save();
-        const user = await User.updateOne({_id: req.userId}, {$push: {todos: todo._id}})
-        res.status(200).send({ message: "Todo was inserted successfully!" });
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({ error: "There was a server side error!" });
-    }
+  try {
+    const newTodo = new Todo({
+      ...req.body,
+      user: req.userId
+    });
+    const todo = await newTodo.save();
+    const user = await User.updateOne({ _id: req.userId }, { $push: { todos: todo._id } })
+    res.status(200).send({ message: "Todo was inserted successfully!" });
+  } catch (err) {
+    console.log(err)
+    res.status(500).send({ error: "There was a server side error!" });
+  }
 })
 
 // POST MULTIPLE TODO
 router.post("/all", async (req, res) => {
-    try {
-        await Todo.insertMany(req.body);
-        res.status(200).send({ message: "Todos were inserted successfully!" })
-    } catch (err) {
-        res.status(500).send({ error: "There was a server side error!" })
-    }
+  try {
+    await Todo.insertMany(req.body);
+    res.status(200).send({ message: "Todos were inserted successfully!" })
+  } catch (err) {
+    res.status(500).send({ error: "There was a server side error!" })
+  }
 })
 
 // PUT TODO
 router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { title, description, status } = req.body;
-  
-    try {
-      const updatedTodo = await Todo.updateOne(
-        { _id: id },
-        { $set: { title, description, status } }
-      );
+  const { id } = req.params;
+  const { title, description, status } = req.body;
 
-      if (updatedTodo.modifiedCount === 0) {
-        return res.status(404).send({ error: 'Todo not found' });
-      }
-  
-      res.status(200).send({ message: 'Todo updated successfully' });
-    } catch (err) {
-        console.error(err);
-      res.status(500).send({ error: 'There was a server side error!' });
+  try {
+    const updatedTodo = await Todo.updateOne(
+      { _id: id },
+      { $set: { title, description, status } }
+    );
+
+    if (updatedTodo.modifiedCount === 0) {
+      return res.status(404).send({ error: 'Todo not found' });
     }
+
+    res.status(200).send({ message: 'Todo updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'There was a server side error!' });
+  }
 })
 
 // DELETE TODO
 router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-      const deletedTodo = await Todo.findByIdAndRemove(id);
-  
-      if (!deletedTodo) {
-        return res.status(404).send({ error: 'Todo not found' });
-      }
-  
-      res.status(200).send({ message: 'Todo deleted successfully' });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: 'There was a server side error!' });
+  try {
+    const deletedTodo = await Todo.findByIdAndRemove(id);
+
+    if (!deletedTodo) {
+      return res.status(404).send({ error: 'Todo not found' });
     }
+
+    res.status(200).send({ message: 'Todo deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'There was a server side error!' });
+  }
 })
 
 module.exports = router;
--- schemas/todoSchema.js
+++ schemas/todoSchema.js
@@ -12,11 +12,33 @@ const todoSchema = mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    }, 
+    },
     user: {
         type: mongoose.Types.ObjectId,
         ref: "User"
     }
 })
+// Instance Methods
+todoSchema.methods = {
+    findActive: function () {
+        return mongoose.model("Todo").find({ status: "active" })
+    },
+    findActiveCallback: function (cb) {
+        return mongoose.model("Todo").find({ status: "active" }, cb)
+    }
+}
 
+// Static Methods
+todoSchema.statics = {
+    findByJs: function () {
+        return this.find({ title: /js/i })
+    }
+}
+
+// Query Helpers
+todoSchema.query = {
+    byLanguage: function (language) {
+        return this.find({ title: new RegExp(language, "i") })
+    },
+}
 module.exports = todoSchema;
